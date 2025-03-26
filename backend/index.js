3f008a3e29f50ce8f45a8099c6d9f0a8944439b9
saksham09 <koulsaksham1@gmail.com>
2025-03-26 17:56:16 +0530

    ready

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+// const express = require('express')// method-1
+import express from "express"; // method-2
+import dotenv from "dotenv"; 
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app,server } from "./socket/socket.js";
+import path from "path";
+dotenv.config({});
+
+ 
+const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
+
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
+const corsOption={
+    origin:'http://localhost:3000',
+    credentials:true
+};
+app.use(cors(corsOption)); 
+
+
+// routes
+app.use("/api/v1/user",userRoute); 
+app.use("/api/v1/message",messageRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+app.get('*',(_,res) => {
+    res.sendFile(path.resolve(_dirname,"frontend","build","index.html"));
+})
+ 
+
+server.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server listen at prot ${PORT}`);
+});
+
